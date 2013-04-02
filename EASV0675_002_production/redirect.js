79b5aa72b0ec553db7c67ef3415eafe5e65d0d44
Tom Watson <thomas@watson.org>
2013-04-02 11:30:51 +0100

    Setup

---
+++ EASV0675_002_production/redirect.js
@@ -0,0 +1,4 @@
+copyPath = "/Users/tomw/Desktop/Clutter/vccp/0017_EasyJet/021_NewAppVideo/scripts/copy.js";
+switcherCopyPath = "/Users/tomw/Desktop/Clutter/vccp/0017_EasyJet/021_NewAppVideo/scripts/switcher_copy.js";
+switcherImgPath = "/Users/tomw/Desktop/Clutter/vccp/0017_EasyJet/021_NewAppVideo/scripts/switcher_img.js";
+datePath = "/Users/tomw/Desktop/Clutter/vccp/0017_EasyJet/021_NewAppVideo/scripts/date.js";
