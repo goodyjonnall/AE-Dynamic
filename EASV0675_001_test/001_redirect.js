5ad8bd561d859791b10226fc0ff72142c9be3ada
Tom Watson <thomas@watson.org>
2013-04-02 10:51:31 +0100

    initial project version

---
+++ EASV0675_001_test/001_redirect.js
@@ -0,0 +1,4 @@
+copyPath = "/Users/tomw/Desktop/Clutter/vccp/000A_research_and_development/0045_language_switched/scripts/copy.js";
+switcherCopyPath = "/Users/tomw/Desktop/Clutter/vccp/000A_research_and_development/0045_language_switched/scripts/switcher_copy.js";
+switcherImgPath = "/Users/tomw/Desktop/Clutter/vccp/000A_research_and_development/0045_language_switched/scripts/switcher_img.js";
+datePath = "/Users/tomw/Desktop/Clutter/vccp/000A_research_and_development/0045_language_switched/scripts/date.js";
